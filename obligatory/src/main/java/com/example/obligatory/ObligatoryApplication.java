c80091e19adc1b1805132d9e3c6751c31556bbf0
IdaNagel <143513463+IdaNagel@users.noreply.github.com>
2024-04-19 11:21:48 +0200

    Add files via upload

---
+++ obligatory/src/main/java/com/example/obligatory/ObligatoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligatoryApplication.class, args);
+    }
+
+}
